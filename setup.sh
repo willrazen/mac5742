90974f5e381422e5b60fe938d9d4e9ed502b1989
Will Razen <16233083+willrazen@users.noreply.github.com>
2022-03-27 23:09:00 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 sudo yum -y update
 
-# show info
-cat /etc/os-release
-cat /proc/version
+# paths
+ENV=/home/ec2-user/environment/mac5742
+cd ${ENV}
+
+# get info
+INFO=env_info.txt
+cat /etc/os-release > ${INFO}
+cat /proc/version >> ${INFO}
+cat /proc/cpuinfo >> ${INFO}
 grep -i bogomips /proc/cpuinfo
 
 # install hyperfine
-VER=v1.13.0-x86_64-unknown-linux-musl
-wget https://github.com/sharkdp/hyperfine/releases/download/v1.13.0/hyperfine-$VER.tar.gz -O hyperfine.tar.gz
-tar -xzvf hyperfine.tar.gz
-mv hyperfine-$VER/ hyperfine/
-git clone https://github.com/sharkdp/hyperfine.git tmp
-mv tmp/scripts/ hyperfine/
-
-# clean up
-rm -rf tmp hyperfine.tar.gz
+VER=v1.13.0
+ARQ=x86_64-unknown-linux-musl
+mkdir -p tmp
+wget https://github.com/sharkdp/hyperfine/releases/download/${VER}/hyperfine-${VER}-${ARQ}.tar.gz -O tmp/hyperfine.tar.gz
+tar -xzvf tmp/hyperfine.tar.gz -C tmp
+mv tmp/hyperfine-${VER}-${ARQ}/* hyperfine/
+git clone https://github.com/sharkdp/hyperfine.git tmp/repo
+mv tmp/repo/scripts/ hyperfine/
+rm -rf tmp
